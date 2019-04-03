431a9fe6d1fac35e6a739a3ecb45726ba7eca942
khushbusinghal333 <khushbusinghal.2007@gmail.com>
2019-04-03 11:58:22 +0530

    all ok

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ $(document).on('click', '.delete', function(){
     console.log(id);
     $(".subscriber"+id).remove();
 })
+
+
+
+function check()
+{
+
+    var pass1 = document.getElementById('mobile');
+
+
+    var message = document.getElementById('message');
+
+   var goodColor = "#0C6";
+    var badColor = "#FF9B37";
+
+    if(mobile.value.length!=10){
+
+        mobile.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "required 10 digits, match requested format!"
+    }}
